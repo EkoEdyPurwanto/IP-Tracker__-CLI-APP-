05dc01dafa0afe1a64559351a8ea0f8928e09e34
EchoEdyP <forsocmedecho@proton.me>
2023-05-23 13:17:38 +0700

    add: struct ip

---
+++ internal/models/ip.go
@@ -0,0 +1,11 @@
+package models
+
+type Ip struct {
+	IP       string `json:"ip"`
+	City     string `json:"city"`
+	Region   string `json:"region"`
+	Country  string `json:"country"`
+	Loc      string `json:"loc"`
+	Timezone string `json:"timezone"`
+	Postal   string `json:"postal"`
+}
